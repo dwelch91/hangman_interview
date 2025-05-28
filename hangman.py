ca834c23116ca35019b99b5866ea00e3195f7c2c
Donald J. Welch <dwelch91@gmail.com>
2025-05-28 07:58:22 -0700

    Create hangman.py

---
+++ hangman.py
@@ -0,0 +1,19 @@
+# Constant values for game status
+STATUS_WIN = 'win'
+STATUS_LOSE = 'lose'
+STATUS_ONGOING = 'ongoing'
+class Hangman:
+    """
+    A minimum implementation for the logic necessary to play the game Hangman.
+    """
+    def __init__(self, word, remaining_guesses=9):
+        pass
+    def guess(self, char):
+        """
+        The player can guess at a character that is in the words.
+        A correct guess should reveal the letter in the masked word.
+        A wrong guess counts against the number of guesses the player has.
+        :param char: The character guessed by a player
+        :return: None
+        """
+        pass
